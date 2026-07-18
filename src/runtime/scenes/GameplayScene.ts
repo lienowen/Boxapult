06b4f38734e247a26d894dd49fd0e436980f52c8
lienowen <lienowen@outlook.com>
2026-07-18 15:35:29 +0800

    Wire first playable scene

---
+++ src/runtime/scenes/GameplayScene.ts
@@ -0,0 +1,108 @@
+import Phaser from 'phaser';
+import type { GameServices } from '../../application/GameServices';
+import { gameBalance } from '../../content/config/gameBalance';
+import { getLevel } from '../../content/levels/levelCatalog';
+import { GameFlow } from '../../domain/flow/GameFlow';
+import { GamePhase } from '../../domain/flow/GamePhase';
+import type { LevelDefinition } from '../../domain/level/LevelDefinition';
+import { FailureSystem } from '../systems/FailureSystem';
+import { GoalSystem } from '../systems/GoalSystem';
+import { LaunchController } from '../systems/LaunchController';
+import { LevelBuilder, type LevelRuntime } from '../systems/LevelBuilder';
+import { GameHud } from '../ui/GameHud';
+
+interface GameplaySceneData {
+  readonly levelId: string;
+}
+
+export class GameplayScene extends Phaser.Scene {
+  #services!: GameServices;
+  #flow!: GameFlow;
+  #level!: LevelDefinition;
+  #runtime!: LevelRuntime;
+  #launch!: LaunchController;
+  #goal!: GoalSystem;
+  #failure!: FailureSystem;
+  #hud!: GameHud;
+  #restartKey: Phaser.Input.Keyboard.Key | null = null;
+
+  constructor() {
+    super('gameplay');
+  }
+
+  init(data: GameplaySceneData): void {
+    this.#level = getLevel(data.levelId);
+  }
+
+  create(): void {
+    this.#services = this.registry.get('services') as GameServices;
+    this.#flow = new GameFlow();
+    this.#flow.moveTo(GamePhase.Loading);
+
+    this.matter.set60Hz();
+    this.matter.world.setBounds(0, 0, 1920, 1080, 64, true, true, true, true);
+
+    const builder = new LevelBuilder(this, gameBalance);
+    this.#runtime = builder.build(this.#level);
+    this.#launch = new LaunchController(
+      this,
+      this.#runtime.package,
+      this.#flow,
+      gameBalance.launch,
+      this.#level.launchPoint,
+    );
+    this.#goal = new GoalSystem(
+      this.#runtime.package,
+      this.#runtime.integrity,
+      this.#level.goal,
+      this.#flow,
+    );
+    this.#failure = new FailureSystem(
+      this.#runtime.package,
+      this.#runtime.integrity,
+      this.#level.worldBounds,
+      gameBalance,
+      this.#flow,
+    );
+    this.#hud = new GameHud(this, this.#level.id, this.#runtime.integrity, this.#flow);
+
+    this.#restartKey = this.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.R) ?? null;
+    this.#restartKey?.on('down', this.#restartLevel, this);
+    this.input.on(Phaser.Input.Events.POINTER_DOWN, this.#handleResultPointer, this);
+
+    this.events.once(Phaser.Scenes.Events.SHUTDOWN, this.#shutdown, this);
+    this.#flow.subscribe((_previous, current) => {
+      if (current === GamePhase.Aiming) {
+        this.#services.platform.gameplayStart();
+      } else if (current === GamePhase.Success || current === GamePhase.Failure) {
+        this.#services.platform.gameplayStop();
+      }
+    });
+
+    this.#flow.moveTo(GamePhase.Aiming);
+  }
+
+  override update(_time: number, delta: number): void {
+    this.#goal.update(delta);
+    this.#failure.update(delta);
+    this.#hud.updateIntegrity(this.#runtime.integrity);
+  }
+
+  readonly #handleResultPointer = (): void => {
+    if (this.#flow.current === GamePhase.Success || this.#flow.current === GamePhase.Failure) {
+      this.#restartLevel();
+    }
+  };
+
+  readonly #restartLevel = (): void => {
+    this.scene.restart({ levelId: this.#level.id });
+  };
+
+  readonly #shutdown = (): void => {
+    this.#services.platform.gameplayStop();
+    this.#restartKey?.off('down', this.#restartLevel, this);
+    this.input.off(Phaser.Input.Events.POINTER_DOWN, this.#handleResultPointer, this);
+    this.#launch.destroy();
+    this.#hud.destroy();
+  };
+}
